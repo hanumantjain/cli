43b57b823e6f11613eb2be0080f215428b394f81
hanumantjain <hanumantjain939@gmail.com>
2024-08-26 15:05:35 -0400

    updates

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -6,7 +6,7 @@ const Resume = () => {
     <div>
         <p className="text-blue-500">Resume :~$ <span className='text-white'>Click below to view download the resume: </span></p>
         <div className='flex justify-evenly pt-10'>
-            <a href={resume} target='_blank' className=' border-white border-b-2'>View Resume</a>
+            <a href={resume} target='_blank' rel="noreferrer" className=' border-white border-b-2'>View Resume</a>
             <a href={resume} download="Hanumant_Resume.pdf" className=' border-white border-b-2'>Download Resume</a>
         </div>
     </div>
